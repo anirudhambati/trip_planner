a4157baaa6e653a3acf7bbcc4d4b522573cc2d2f
dumpalasaiavinash <36899352+dumpalasaiavinash@users.noreply.github.com>
2020-02-26 00:43:21 +0530

    email verification

--- trip_planner/urls.py
+++ trip_planner/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from home import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +10,6 @@ urlpatterns = [
     path('about/', views.about, name="about"),   
     path('auth/', views.auth, name="auth"),
     path('registration/', include(('home.urls','home'))),
+    url(r'activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
+    views.activate, name='activate'),
 ]
