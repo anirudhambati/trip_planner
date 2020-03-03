b8813d92eb2b789ac056fd181af0bdc842f011d2
Anirudh Ambati <anirudhambati@gmail.com>
2020-03-03 23:14:53 +0530

    graphing

--- home/plan.py
+++ home/plan.py
@@ -1,3 +1,8 @@
+# from itertools import combinations
+# import math
+# import networkx as nx
+# from graph import *
+#
 # def plan(place, days, questions):
 #
 #     if place == country:
@@ -9,10 +14,24 @@
 #
 #     return plan
 #
-#
-#
 # def plan_city(city, days, questions):
-#     places, time = get_places(city, days, questions)
+#     places, times = get_places(city, days, questions)
+#     time_counter = 0
+#     total_time = days * 7
+#     coords = []
+#     final_places, final_times = [], []
+#     for place, time in zip(places, times):
+#         time_counter = time_counter + time
+#         if time_counter >= total_time:
+#             break
+#         else:
+#             final_places.append(place)
+#             final_times.append(time)
+#             coords.append(get_coords(place))
+#             continue
+#
+#     G = get_graph(coords)
+#     plan = dijkstra(G)
 #
 #     return plan
 #
@@ -24,4 +43,6 @@
 # get_places
 # -----------------------
 # plan_city
+# -----------------------
+# get_coords
 # '''
