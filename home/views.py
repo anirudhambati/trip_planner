0608b6cf5644b8a8840a0fe0b4e5333c550e1f20
Anirudh Ambati <anirudhambati@gmail.com>
2020-07-21 20:45:12 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -37,8 +37,8 @@ config = {
 }
 
 firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
-
+authe = firebase.auth()
+database = firebase.database()
 countries = [
     "Afghanistan",
     "Åland Islands",
@@ -494,8 +494,7 @@ def login(request):
         return redirect('auth')
 
     try:
-        user = auth.sign_in_with_email_and_password(email, password)
-        return redirect('home')
+        user = authe.sign_in_with_email_and_password(email, password)
     except:
         if(email != '' and password!=''):
             messages.success(request, 'Failed to login as the email or password does not match.')
@@ -504,12 +503,18 @@ def login(request):
             messages.success(request, 'Failed to login as the email or password is provided empty')
             return redirect('auth')
 
+    session_id = user['idToken']
+    request.session['uid'] = str(session_id)
+
+    return redirect('home')
 
 def signup(request):
     username = request.POST.get('username')
     email = request.POST.get('email')
     password = request.POST.get('password')
     re_password = request.POST.get('repassword')
+
+    user = authe.create_user_with_email_and_password(email, password)
 
     if (username != '' and email != '' and password != '' and re_password != ''):
         if (password == re_password):
